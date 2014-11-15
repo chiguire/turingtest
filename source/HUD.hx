87fded79ff9af12412c58f411cf2408db72fc64e
Stelios <avramids@gmail.com>
2014-11-15 21:08:41 +0000

    Hud cllass again yeaaah!

--- source/HUD.hx
+++ source/HUD.hx
@@ -12,18 +12,28 @@ using flixel.util.FlxSpriteUtil;
 class HUD extends flixel.group.FlxTypedGroup<FlxSprite>
 {
 
-    private var overlay:FlxSpriteGroup;
-	public var healthDisplay:FlxText;
+	
+	public var move_right:FlxText;
+	public var move_left:FlxText;
+	public var move_up:FlxText;
+	public var move_down:FlxText;
+	
+	public var arrow_up:FlxSprite;
 
-	public function create():Void 
-	{
-		healthDisplay = new FlxText(200, 200, 500);
-		healthDisplay.size = 50;
-		healthDisplay.text = "This is a test";
+	public function new()
+	{	 
+		super();
+		//move_right = new FlxText(25, 25, 30, "Right", 15);
+		//move_left= new FlxText(25, 25, 30, "Left", 15);
+		//move_up = new FlxText(25, 25, 30, "Up", 15);
+		move_down = new FlxText(FlxG.width - 110 , 10, 120);
+		move_down.text = "Down";
+		move_down.size = 30; 
+		add(move_down);
+		//add(move_right);
+		//add(move_left);
+		//add(move_up);
+		
 	}
 	
-    public function updateHealth():Void
-    {
-		healthDisplay.text = "I am updated";
-    }
 }
