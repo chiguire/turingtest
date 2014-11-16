eecb0c2fc9e999a550eb094718b92da47029d772
Ciro Durán <ciro.duran@gmail.com>
2014-11-16 17:28:20 +0000

    New ballroom Add Douglas to credits

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -25,7 +25,7 @@ class CreditsState extends FlxState
 		var logo : FlxSprite = new FlxSprite(FlxG.width/2 - 562/2, -10, AssetPaths.logo__png);
 		add(logo);
 		
-		var le_text : FlxText = new FlxText(FlxG.width / 2 - 300, 190, 600, "Thanks for playing \"There are no vampires here\". If you find anything that does not cater to your standards of satisfaction, please let Management know.\n\nCredits:\n\nStelios Avramidis - Stupendous Programming\nCiro Durán - Bombastic Programming\nMary Safro - Overwhelming Art\n\nStelios: For my parents / Ciro: For .Y. / All: For Simon Franco\nMade for the Franco Game Jam, at The Creative Assembly.\nHorsham, November 2014.", 16, true);
+		var le_text : FlxText = new FlxText(FlxG.width / 2 - 300, 190, 600, "Thanks for playing \"There are no vampires here\". If you find anything that does not cater to your standards of satisfaction, please let Management know.\n\nCredits:\n\nStelios Avramidis - Stupendous Programming\nCiro Durán - Bombastic Programming\nMary Safro - Overwhelming Art\nDouglas Pennants - Explosive Sound\n\nStelios: For my parents / Ciro: For .Y. / All: For Simon Franco\nMade for the Franco Game Jam, at The Creative Assembly.\nHorsham, November 2014.", 12, true);
 		add(le_text);
 		
 		var le_button : FlxUIButton = new FlxUIButton(FlxG.width / 2 - 250, FlxG.height - 40, "Go back and play!", go_back);
